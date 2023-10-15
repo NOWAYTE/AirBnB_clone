9d43e5e3ef1accf10c897f4307d52f7c598716af
Daniel kamau nganga <danielkamaunganga3@gmail.com>
2023-10-15 17:56:34 +0300

    Update the base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Module containing BaseClass"""
+""" Module containing BaseModel class """
 
 
 import uuid
@@ -7,19 +7,32 @@ from datetime import datetime
 
 class BaseModel:
     """BaseModel class"""
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != '__class__':
+                    setattr(self, key, value)
+        else:
+
+            self.id = str(uuid.uuid4())
+
+            self.created_at = datetime.now()
+
+            self.updated_at = self.created_at
 
     def save(self):
         self.updated_at = datetime.now()
 
     def to_dict(self):
         obj_dict = self.__dict__.copy()
+
         obj_dict['__class__'] = self.__class__.__name__
         obj_dict['created_at'] = self.created_at.isoformat()
+
         obj_dict['updated_at'] = self.updated_at.isoformat()
+
         return obj_dict
 
     def __str__(self):
