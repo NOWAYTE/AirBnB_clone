51ff3aa908beac284f879ae83725f84067bdfa18
Daniel kamau nganga <danielkamaunganga3@gmail.com>
2023-10-15 16:19:30 +0300

    A BaseModel that defines all common attributes/methods for other classes

---
+++ models/__init__.py
@@ -0,0 +1,10 @@
+#Create a Filestorage instance for the application
+
+from models.engine.file_storage import Filestorage
+
+
+#An instance of a FileStorage
+
+storage = FileStorage()
+storage.reload()
+
