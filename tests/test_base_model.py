4a4e92cea878081addc6b2816b01cc7c1ab7f095
NOWAYTE <danielkamaunganga3@gmail.com>
2024-07-28 12:17:35 +0300

    Fix pycodestyle error

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -21,6 +21,7 @@ class TestBaseModel(unittest.TestCase):
         self.assertNotEqual(instance1, instance2)
         self.assertIsInstance(instance1.id, str)
         self.assertEqual(len(instance1.id), 36)
+
     def test_datetime_att(self):
         """
         Test that two BaseModel instances have different
@@ -38,12 +39,12 @@ class TestBaseModel(unittest.TestCase):
         time = "%Y-%m-%dT%H:%M:%S.%f"
         instance = BaseModel()
         n_dict = instance.to_dict()
-
-        self.assertEqual(type(n_dict['created_at']), str)
         self.assertEqual(type(n_dict['updated_at']), str)
-        self.asserEqual(n_dict['__class__'], "BaseModel")
-        self.assertEqual(n_dict['created_at'], instance.created_at.strftime(time))
-        self.assertEqual(n_dict['updated_at'], instance.updated_at.strftime(time))
+        self.assertEqual(n_dict['__class__'], "BaseModel")
+        self.assertEqual(
+                n_dict['updated_at'],
+                instance.updated_at.isoformat()
+                )
 
 
 if __name__ == "__main__":
