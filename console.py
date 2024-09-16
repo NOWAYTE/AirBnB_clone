c87a289195ad6192a5cc5c246466fad89f509c38
NOWAYTE <danielkamaunganga3@gmail.com>
2024-09-16 23:52:08 +0300

    Update

--- console.py
+++ console.py
@@ -1,38 +1,35 @@
 #!/usr/bin/python3
 """Command line interpreter"""
-
-import cmd, sys
-
+import cmd
+import sys
 
 
 class HBNBCommand(cmd.Cmd):
+    """Command line intrepreter"""
+
     intro = "Welcome to my AirBnB console, Type help or ? to list commands \n"
     prompt = '(HBNB)'
     file = None
 
-
-
     def do_EOF(self, line):
         """Exit interpreter"""
+
         return True
 
     def postcmd(self, stop, line):
         """Clean up after exiting"""
+
         return stop
 
     def do_quit(self, line):
         """Quit the command line intrepreter"""
+
         print("Cheers !!")
 
         return True
 
 
-
-
-
-
-
-
-
 if __name__ == "__main__":
+    """"""
+
     HBNBCommand().cmdloop()
