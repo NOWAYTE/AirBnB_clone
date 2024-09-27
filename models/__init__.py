48e39099c558df49b0f3ad332c1d4222a78419f3
Daniel kamau <danielkamaunganga3@gmail.com>
2024-09-27 18:05:58 +0300

    Add state city amenity place and review class

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,25 @@
 #!/usr/bin/python3
 
 from models.engine.file_storage import FileStorage
-
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State  # Import the State class
+from models.city import City  # Import the City class
+from models.amenity import Amenity  # Import the Amenity class
+from models.place import Place  # Import the Place class
+from models.review import Review  # Import the Review class
 
 storage = FileStorage()
 storage.reload()
+
+# Add to the classes dictionary
+classes = {
+    'BaseModel': BaseModel,
+    'User': User,
+    'State': State,  # Add State class
+    'City': City,  # Add City class
+    'Amenity': Amenity,  # Add Amenity class
+    'Place': Place,  # Add Place class
+    'Review': Review,  # Add Review class
+}
+
---
+++ models/state.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+from models.base_model import BaseModel
+
+class State(BaseModel):
+    """State class that inherits from BaseModel."""
+
+    name = ""
+
