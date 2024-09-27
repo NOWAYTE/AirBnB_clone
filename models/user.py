c907aa1bd96d4a9603f33ee715485f711e0474b1
Daniel kamau <danielkamaunganga3@gmail.com>
2024-09-27 17:58:36 +0300

    Update User

--- models/user.py
+++ models/user.py
@@ -1,36 +1,13 @@
 #!/usr/bin/python3
-""" holds User"""
+"""Holds class user"""
 
-import models
-from models.base_model import BaseModel, Base
-from os import getenv
-import sqlalchemy
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
-import hashlib
+from models.base_model import BaseModel
 
+class User(BaseModel):
+    """User class that inherits from BaseModel."""
+    
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
 
-class User(BaseModel, Base):
-    """Representation of a user """
-    if models.storage_t == 'db':
-        __tablename__ = 'users'
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
-        places = relationship("Place", backref="user")
-        reviews = relationship("Review", backref="user")
-    else:
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
-
-    def __init__(self, *args, **kwargs):
-        """initializes user"""
-        if 'password' in kwargs:
-            password = kwargs['password']
-            m = hashlib.md5()
-            m.update(str.encode(password))
-            kwargs['password'] = m.hexdigest()
-        super().__init__(*args, **kwargs)
